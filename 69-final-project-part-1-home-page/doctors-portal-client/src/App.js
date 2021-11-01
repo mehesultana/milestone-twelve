b19a368000222838fe0453ed9143f658d1ff8d33
mehesultana <sultanamehe01@gmail.com>
2021-11-02 02:35:24 +0600

    Add Dynamic background image for the appointment

--- 69-final-project-part-1-home-page/doctors-portal-client/src/App.js
+++ 69-final-project-part-1-home-page/doctors-portal-client/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
+import './App.css';
 
 function App() {
 	return (
-		<div>
+		<div className="App">
 			<Router>
 				<Switch>
 					<Route>
