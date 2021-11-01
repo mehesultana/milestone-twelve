22a32423945f40cf86d41b0676ad17adf8c54e0e
mehesultana <sultanamehe01@gmail.com>
2021-11-02 00:45:54 +0600

    project setup

---
+++ 69-final-project-part-1-home-page/doctors-portal-client/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+
+function App() {
+	return (
+		<div>
+			<Router>
+				<Switch>
+					<Route>
+						<Home path="/home" />
+					</Route>
+					<Route>
+						<Home path="/" />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
+}
+
+export default App;
